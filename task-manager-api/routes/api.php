fb85f2f7da14151a6ed1780164dbd7d9a1c24833
petermagaga <ptrmagaga@gmail.com>
2026-03-31 21:44:23 +0300

    debug_db task manager

--- task-manager-api/routes/api.php
+++ task-manager-api/routes/api.php
@@ -2,6 +2,7 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\TaskController;
+use Illuminate\support\Facades\DB;
 
 Route::prefix('tasks')->group(function () {
     Route::post('/', [TaskController::class, 'store']);
@@ -9,5 +10,13 @@ Route::prefix('tasks')->group(function () {
     Route::patch('{id}/status', [TaskController::class, 'updateStatus']);
     Route::delete('{id}', [TaskController::class, 'destroy']);
     Route::get('report', [TaskController::class, 'report']);
+
+    Route::get('/debug-db', function () {
+        return [
+            'connection' => config('database.default'),
+            'database' => config('database.connections.mysql.database'),
+            'host' => config('database.connections.mysql.host'),
+            'count' => \App\Models\Task::count()
+        ];
 
 });
